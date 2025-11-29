444476e9eea80c47b047371128c865761a632d62
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-29 22:01:31 +0700

    Add files via upload

---
+++ bài 2/bài 2, 1.4.1.py
@@ -0,0 +1,9 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+
+# Sửa lỗi: Cần dùng dấu hai chấm (:) và toán tử so sánh đúng
+if n1 >= n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
