63849c50337e51bd29456a15de709e6e1b1450fd
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:42:59 +0700

    Add files via upload

---
+++ Bài 4/bài 4.11.py
@@ -0,0 +1,13 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+ds = ['a', 'b', 'c'] # Giả định list ban đầu
+print(f"List ban đầu: {ds}")
+
+# Thêm phần tử 'abc' vào cuối list
+ds.append('abc')
+
+print(f"List sau khi thêm: {ds}") 
+# Output: ['a', 'b', 'c', 'abc']
+
+# In từng phần tử
+# for ch in ds:
+#     print(ch)
