53c8674a95d082856b264fb7c13b7716d3bcc28e
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:51:47 +0700

    Add files via upload

---
+++ Bài 5/bài 5.4.py
@@ -0,0 +1,9 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+import numpy as np
+
+x = np.arange(12, 39) 
+print("Mảng ban đầu:", x)
+
+# Đảo ngược mảng bằng slicing [::-1]
+x_reversed = x[::-1]
+print("Mảng đảo ngược:", x_reversed)
