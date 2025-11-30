4dff1a93d264fe1a461c3557c5c9495876d7982d
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:30:33 +0700

    Add files via upload

---
+++ bài 2/bài 2, 1.4.11.py
@@ -0,0 +1,14 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+l=[1,'python',4,7]
+k=['cse',2,'guntur',8]
+m=[] # Danh sách m rỗng
+
+# Nối các phần tử vào danh sách m
+m.append(l);
+m.append(k);
+print("Danh sách m sau khi append l và k:", m); 
+# Output: m = [[1, 'python', 4, 7], ['cse', 2, 'guntur', 8]]
+
+# Tạo dictionary từ các biến
+d={'l':l, 'k':k, 'combine_list':m}
+print("Dictionary d:", d)
