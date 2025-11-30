b60e171cd4e4ff7efe75bbb94f37051d696fd11e
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:33:03 +0700

    Add files via upload

---
+++ Bài 3/bài 3.1.py
@@ -0,0 +1,10 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+def sum_no_return(a, b):
+    # Tính tổng và in ra màn hình
+    print("sum = " + str(a + b))
+
+# Gọi hàm
+# Tính tổng 2 số 4, 5
+sum_no_return(4, 5) # Output: sum = 9
+# Tính tổng 2 số 3, 7
+sum_no_return(3, 7) # Output: sum = 10
