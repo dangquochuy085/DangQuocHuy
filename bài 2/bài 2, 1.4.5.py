d51134eda9c1d8ac952affb9593e1bd0177a5be0
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:27:43 +0700

    Add files via upload

---
+++ bài 2/bài 2, 1.4.5.py
@@ -0,0 +1,7 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+n = int(input("Enter A number--->"))
+# Đảm bảo n >= 0 trước khi vào vòng lặp
+if n >= 0:
+    while n >= 0:
+        print(n)
+        n = n - 1
