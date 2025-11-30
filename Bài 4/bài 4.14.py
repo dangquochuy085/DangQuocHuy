38fea020a525c438334537fe7f16513eaae94841
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:44:00 +0700

    Add files via upload

---
+++ Bài 4/bài 4.14.py
@@ -0,0 +1,13 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+ds = ['b', 'd', 'a', 'c'] # Giả định list ban đầu
+print(f"List ban đầu: {ds}")
+
+# Sắp xếp các phần tử theo thứ tự tăng dần (Alphabetical cho chuỗi)
+ds.sort()
+
+print(f"List sau khi sắp xếp: {ds}")
+# Output: ['a', 'b', 'c', 'd']
+
+# In từng phần tử
+# for ch in ds:
+#     print(ch)
