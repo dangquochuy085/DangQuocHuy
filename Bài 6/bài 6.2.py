f1e4ad57f23642142dd43c76769f6f65e4336c0b
dangquochuy085 <dangquochuy085@gmail.com>
2025-11-30 11:56:13 +0700

    Add files via upload

---
+++ Bài 6/bài 6.2.py
@@ -0,0 +1,14 @@
+print("Dang Quoc Huy\nMSV:245752021610171");
+class Hinhchunhat:
+    # Phương thức khởi tạo
+    def __init__(self, chieudai, chieurong):
+        self.dai = chieudai
+        self.rong = chieurong
+        
+    # Phương thức tính diện tích (Area)
+    def area(self):
+        return self.dai * self.rong
+
+# Kiểm tra
+hcn1 = Hinhchunhat(10, 5)
+print(f"Hình chữ nhật có chiều dài 10, rộng 5. Diện tích: {hcn1.area()}")
